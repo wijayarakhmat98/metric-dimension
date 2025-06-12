c2c9f50b2ce50114e15838358f40f5ebf7fb0a94
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2025-06-12 20:54:01 +0000

    Fix finding metric dimension

--- example.py
+++ example.py
@@ -3,22 +3,6 @@
 import graph_utils
 import metric_dimension
 import numpy as np
-import z3
-
-def find_least(v, constraint):
-	count = z3.Sum(*[z3.If(b, 0, 1) for b in v])
-	o = z3.Optimize()
-	o.add(constraint)
-	o.minimize(count)
-	if o.check() != z3.sat:
-		return z3.unsat, -1, []
-	model = o.model()
-	w = [b for b in v if z3.is_false(model.evaluate(b))]
-	return z3.sat, len(w), w
-
-def get_resolving_representation(v, d, w):
-	mask = np.array([b in w for b in v])
-	return d[:, mask]
 
 def main():
 	m = np.array([
@@ -28,14 +12,25 @@ def main():
 		[0, 1, 1, 0, 0],
 		[0, 1, 0, 0, 0]
 	])
-	v = metric_dimension.create_node_boolean(m)
+	print(m, '\n')
 	d = graph_utils.distance_matrix(m)
+	print(d, '\n')
+	v = metric_dimension.create_node_boolean(m)
+	print(v, '\n')
 	p = metric_dimension.distance_similarity_prune(d)
-	r = metric_dimension.apply_boolean_similarity(v, p)
-	c = z3.Not(z3.Or(*r))
-	sat, n, w = find_least(v, c)
-	z = get_resolving_representation(v, d, w)
-	print(m, v, d, p, c, sat, n, w, z, sep='\n\n')
+	print(p, '\n')
+	t = metric_dimension.distance_similarity_permute(p)
+	print(t, '\n')
+	u = metric_dimension.distance_similarity_group(t)
+	for i, c in enumerate(u):
+		print(i)
+		print(c, '\n')
+	found, w = metric_dimension.find_least(v, u)
+	print(found, w)
+	if found:
+		r = metric_dimension.resolving_representation(v, w, d)
+		print(r)
+		print(metric_dimension.valid(r))
 
 if __name__ == '__main__':
 	main()
--- metric_dimension.py
+++ metric_dimension.py
@@ -3,28 +3,66 @@
 import numpy as np
 import z3
 
-def distance_similarity_broadcast(d):
-	return d[None, :, :] == d[:, None, :]
-
-def distance_similarity_mask(d):
-	n = d.shape[0]
-	return np.broadcast_to(
-		(np.arange(n)[None, :] > np.arange(n)[:, None])[:, :, None],
-		(n, n, n)
-	)
-
-def distance_similarity_prune(d):
-	n = d.shape[0]
-	broadcast = distance_similarity_broadcast(d)
-	mask = distance_similarity_mask(d)
-	prune = broadcast[mask].reshape(-1, n)
-	prune = np.unique(prune, axis=0)
-	prune = prune[np.any(prune, axis=1)]
-	return prune
-
 def create_node_boolean(m):
 	n = m.shape[0]
 	return np.array([z3.Bool('x{}'.format(i+1)) for i in range(n)])
 
-def apply_boolean_similarity(v, p):
-	return [z3.And(*v[mask]) for mask in p]
+def distance_similarity_broadcast(d):
+	return d[None, :, :] == d[:, None, :]
+
+def distance_similarity_prune(d):
+	p = distance_similarity_broadcast(d)
+	p = p.reshape(-1, p.shape[2])
+	p = np.unique(p[np.any(p, axis=1) & ~np.all(p, axis=1)], axis=0)
+	return p
+
+def distance_similarity_permute(p):
+	t = []
+	for c in p:
+		idx = np.where(c)[0]
+		n = len(idx)
+		mask = np.arange(2**n)[:, None] & (1 << np.arange(n)) == 0
+		a = np.tile(c, (2**n, 1))
+		a[np.arange(2**n)[:, None], idx] &= mask
+		t.append(a)
+	t = np.vstack(t)
+	t = np.unique(t[np.any(t, axis=1)], axis=0)
+	return t
+
+def distance_similarity_group(t):
+	u = [[] for _ in range(t.shape[1] + 1)]
+	for c in t:
+		u[np.sum(c)].append(c)
+	for i in range(len(u)):
+		if len(u[i]) > 0:
+			u[i] = np.vstack(u[i])
+	return u
+
+def apply_boolean_similarity(v, g):
+	return [z3.And(*v[c]) for c in g]
+
+def find_exact(v, u, n):
+	s = z3.Solver()
+	s.add(z3.Not(z3.Or(apply_boolean_similarity(v, u[n]))))
+	s.add(z3.AtLeast(*v, n))
+	s.add(z3.AtMost(*v, n))
+	if s.check() == z3.unsat:
+		return False, []
+	model = s.model()
+	w = [b for b in v if z3.is_true(model.evaluate(b))]
+	return True, w
+
+def find_least(v, u):
+	for i in range(1, len(u)):
+		found, w = find_exact(v, u, i)
+		if found:
+			return True, w
+	return False, []
+
+def resolving_representation(v, w, d):
+	return d[:, np.array([b in w for b in v])]
+
+def valid(r):
+	unique, idx = np.unique(r, axis=0, return_index=True)
+	unique = unique[np.argsort(idx)]
+	return np.array_equal(r, unique)
