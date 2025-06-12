f0653bd3c4db7eb302e206a89682ff03e7f3ac3c
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2025-06-12 14:45:23 +0000

    Prune distance similarity

--- metric_dimension.py
+++ metric_dimension.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3.13
 
-def distance_similarity(d):
+import numpy as np
+
+def distance_similarity_broadcast(d):
 	return d[None, :, :] == d[:, None, :]
+
+def distance_similarity_mask(d):
+	n = d.shape[0]
+	return np.broadcast_to(
+		(np.arange(n)[None, :] > np.arange(n)[:, None])[:, :, None],
+		(n, n, n)
+	)
+
+def distance_similarity_prune(d):
+	n = d.shape[0]
+	broadcast = distance_similarity_broadcast(d)
+	mask = distance_similarity_mask(d)
+	return broadcast[mask].reshape(-1, n)
