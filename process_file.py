2969d7b74a364fb78b1ca7ec95e7013809460736
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2025-07-06 14:51:20 +0000

    Update process file

--- process_file.py
+++ process_file.py
@@ -16,14 +16,35 @@ def find_metric_dimension(s):
 	start = perf_counter()
 	m = graph_utils.graph6_decode(s)
 	d = graph_utils.distance_matrix(m)
+	e, _ = graph_utils.edge_distance_matrix(m, d)
 	v = metric_dimension.create_node_boolean(m)
 	b = metric_dimension.distance_similarity_broadcast(d)
+	eb = metric_dimension.distance_similarity_broadcast(e)
 	p = metric_dimension.distance_similarity_prune(b)
+	ep = metric_dimension.distance_similarity_prune(eb)
 	t = metric_dimension.distance_similarity_permute(p)
+	et = metric_dimension.distance_similarity_permute(ep)
 	u = metric_dimension.distance_similarity_group(t)
-	_, w = metric_dimension.find_least(v, u)
+	eu = metric_dimension.distance_similarity_group(et)
+	w_start = perf_counter()
+	w = metric_dimension.find_least(v, u)
+	w_end = perf_counter()
+	ew_start = perf_counter()
+	ew = metric_dimension.find_least(v, eu)
+	ew_end = perf_counter()
 	end = perf_counter()
-	return (s, len(w), end - start)
+	return {
+		'graph': s,
+		'vertex': {
+			'dimension': len(w),
+			'time': w_end - w_start
+		},
+		'edge': {
+			'dimension': len(ew),
+			'time': ew_end - ew_start
+		},
+		'time': end - start
+	}
 
 def main(args):
 	if len(args) != 1:
