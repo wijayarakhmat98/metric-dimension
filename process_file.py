8e9086840e72f6a6e38250e50cef991685ca5ed0
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2025-07-06 16:19:19 +0000

    Add enumerate to process file

--- process_file.py
+++ process_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3.13
 
+import copy
 import graph_utils
+import json
 import metric_dimension
 import multiprocessing
+import re
 import sys
 from time import perf_counter
 
@@ -29,35 +32,67 @@ def find_metric_dimension(s):
 	w_start = perf_counter()
 	w = metric_dimension.find_least(v, u)
 	w_end = perf_counter()
+	n_start = perf_counter()
+	n = metric_dimension.find_enumerate(v, u, len(w))
+	n_end = perf_counter()
+	nr = [metric_dimension.resolving_representation(v, w, d) for w in n]
+	nc = [metric_dimension.valid(r) for r in nr]
+	ns = [{'set': [str(x) for x in w], 'is_sane': c} for w, c in zip(n, nc)]
 	ew_start = perf_counter()
 	ew = metric_dimension.find_least(v, eu)
 	ew_end = perf_counter()
 	end = perf_counter()
-	return {
+	en_start = perf_counter()
+	en = metric_dimension.find_enumerate(v, eu, len(ew))
+	en_end = perf_counter()
+	enr = [metric_dimension.resolving_representation(v, ew, e) for ew in en]
+	enc = [metric_dimension.valid(er) for er in enr]
+	ens = [{'set': [str(x) for x in ew], 'is_sane': ec} for ew, ec in zip(en, enc)]
+	info = {
 		'graph': s,
 		'vertex': {
 			'dimension': len(w),
-			'time': w_end - w_start
+			'time': w_end - w_start,
+			'enumerate': {
+				'n': len(n),
+				'resolving': ns,
+				'time': n_end - n_start
+			}
 		},
 		'edge': {
 			'dimension': len(ew),
-			'time': ew_end - ew_start
+			'time': ew_end - ew_start,
+			'enumerate': {
+				'n': len(en),
+				'resolving': ens,
+				'time': en_end - en_start
+			}
 		},
 		'time': end - start
 	}
+	infos = copy.deepcopy(info)
+	infos['vertex']['enumerate']['resolving'] = [str(s) for s in infos['vertex']['enumerate']['resolving']]
+	infos['edge']['enumerate']['resolving'] = [str(s) for s in infos['edge']['enumerate']['resolving']]
+	infos = json.dumps(infos, indent=2)
+	infos = re.sub(r'"({\'set.*)"', r'\1', infos)
+	return {'raw': info, 'str': infos}
 
 def main(args):
-	if len(args) != 1:
+	if len(args) != 2:
 		print('usage:')
-		print('\t<graph6 filename>')
+		print('\t<graph6 filename> <output_filename>')
 		return
 
-	filename = args[0]
-	ss = file_to_list(filename)
+	graph6_filename = args[0]
+	output_filename = args[1]
 
-	with multiprocessing.Pool() as pool:
-		for result in pool.imap_unordered(find_metric_dimension, ss):
-			print(result)
+	ss = file_to_list(graph6_filename)
+
+	with open(output_filename, 'w') as output_file:
+		with multiprocessing.Pool() as pool:
+			for result in pool.imap_unordered(find_metric_dimension, ss):
+				print(result['str'])
+				print(result['raw'], file=output_file)
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
