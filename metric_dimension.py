8da9ba83f19d31a7c81142ce5e27b415a45c7023
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2025-07-16 00:10:06 +0000

    Use optimize minimize

--- metric_dimension.py
+++ metric_dimension.py
@@ -5,7 +5,7 @@ import z3
 
 def create_node_boolean(m):
 	n = m.shape[0]
-	return np.array([z3.Bool('x{}'.format(i+1)) for i in range(n)])
+	return np.array([z3.Int('x{}'.format(i+1)) for i in range(n)])
 
 def distance_similarity_broadcast(d):
 	return d[None, :, :] == d[:, None, :]
@@ -15,37 +15,28 @@ def distance_similarity_prune(b):
 	p = p[np.any(p, axis=1) & ~np.all(p, axis=1)]
 	return p
 
-def apply_boolean_similarity(v, g, n):
-	return z3.And([z3.Not(z3.PbEq([(b, 1) for b in v[c]], n)) for c in g])
-
-def find_exact(v, u, n):
-	s = z3.Solver()
-	s.add(apply_boolean_similarity(v, u, n))
-	s.add(z3.AtLeast(*v, n))
-	s.add(z3.AtMost(*v, n))
-	if s.check() == z3.unsat:
-		return []
-	model = s.model()
-	w = [b for b in v if z3.is_true(model.evaluate(b))]
-	return w
-
 def find_least(v, u):
-	for i in range(1, len(v)):
-		w = find_exact(v, u, i)
-		if len(w) != 0:
-			return w
-	return []
+	s = z3.Optimize()
+	s.add(z3.And([z3.And(b >= 0, b <= 1) for b in v]))
+	k = z3.Int('k')
+	s.add(z3.Sum(*v) == k)
+	s.add(k >= 1)
+	s.add(z3.And([z3.Sum(*v[c]) < k for c in u]))
+	s.minimize(k)
+	s.check()
+	model = s.model()
+	return [b for b in v if model.evaluate(b).as_long() == 1]
 
 def find_enumerate(v, u, n):
 	s = z3.Solver()
-	s.add(apply_boolean_similarity(v, u, n))
-	s.add(z3.AtLeast(*v, n))
-	s.add(z3.AtMost(*v, n))
+	s.add(z3.And([z3.And(b >= 0, b <= 1) for b in v]))
+	s.add(z3.Sum(*v) == n)
+	s.add(z3.And([z3.Sum(*v[c]) < n for c in u]))
 	ws = []
 	while s.check() == z3.sat:
 		model = s.model()
-		w = [b for b in v if z3.is_true(model.evaluate(b))]
-		s.add(z3.Not(z3.And(w)))
+		w = [b for b in v if model.evaluate(b).as_long() == 1]
+		s.add(z3.Not(z3.And([b == 1 for b in w])))
 		ws.append(w)
 	return ws
 
