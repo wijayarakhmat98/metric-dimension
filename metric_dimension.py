e4b415f729d04a952a07885c00cc9f9a866241d9
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2025-07-16 16:28:21 +0000

    Merge debug and process into metric_dimension

--- metric_dimension.py
+++ metric_dimension.py
@@ -1,11 +1,20 @@
+#!/usr/bin/env python3.12
+
 import builtins
+import json
+import multiprocessing
 import networkx as nx
 import numpy as np
 import numpy.typing as npt
 import numba as nb # type: ignore
+import re
 import scipy as sp # type: ignore
-from typing import Any, cast, List, Tuple
+import sys
+from typing import Any, cast, List, TextIO, Tuple
 import z3 # type: ignore
+
+def read_file(file : TextIO) -> List[str]:
+	return [line.strip() for line in file]
 
 def graph6_decode(s : str) -> Tuple[int, npt.NDArray[np.bool_]]:
 	b = s.encode()
@@ -42,18 +51,6 @@ def __njit__distance_matrix_edge(
 		for r, (j, k) in builtins.enumerate(es):
 			de[r, i] = min(d[j, i], d[k, i])
 	return de
-
-__njit__distance_matrix_edge = nb.njit( # type: ignore
-	fastmath=True,
-	cache=True
-)(__njit__distance_matrix_edge)
-
-__njit__distance_matrix_edge.compile(( # type: ignore
-	nb.types.intp,
-	nb.types.Array(nb.types.boolean, 2, 'C'),
-	nb.types.Array(nb.types.intp, 2, 'C'),
-	nb.types.Array(nb.types.intp, 2, 'C')
-))
 
 def distance_matrix_edge(
 	n : int,
@@ -104,7 +101,8 @@ def enumerate(vs : npt.NDArray[Any], p : npt.NDArray[np.bool_], n : int) -> npt.
 		w = [cast(int, model[v].as_long()) for v in vs] # type: ignore
 		z.add(z3.Or([v != v_ for v, v_ in zip(vs, w)])) # type: ignore
 		ws.append(w)
-	return np.array(ws, dtype=np.bool_)
+	ws_ = np.array(ws, dtype=np.bool_)
+	return ws_
 
 def resolving_representation(w : npt.NDArray[np.bool_], d : npt.NDArray[np.int_]) -> npt.NDArray[np.int_]:
 	return d[:, w]
@@ -113,3 +111,136 @@ def is_resolving_valid(r : npt.NDArray[np.int_]) -> bool:
 	r_ = np.unique(r, axis=0)
 	valid = r.shape[0] == r_.shape[0]
 	return valid
+
+def main_debug(graph : str, info : List[str]) -> None:
+	n, m = graph6_decode(graph)
+	vs = variable(n)
+
+	d = distance_matrix(m)
+	p = distance_similarity(d)
+	b = find(vs, p)
+	ws = enumerate(vs, p, b)
+	rs = np.stack([resolving_representation(w, d) for w in ws])
+	rs_valid = np.array([is_resolving_valid(r) for r in rs]).reshape(-1, 1)
+
+	de, es = distance_matrix_edge(n, m, d)
+	pe = distance_similarity(de)
+	be = find(vs, pe)
+	wes = enumerate(vs, pe, be)
+	res = np.stack([resolving_representation(we, de) for we in wes])
+	res_valid = np.array([is_resolving_valid(re) for re in res]).reshape(-1, 1)
+
+	graph = graph6_encode(m)
+	es = np.array([[vs[e[0]], vs[e[1]]] for e in es])
+	ws = np.stack([vs[w] for w in ws])
+	wes = np.stack([vs[we] for we in wes])
+
+	log : List[str] = []
+	for i in info:
+		match i:
+			case 'graph'    : log.append(graph)
+			case 'n'        : log.append(str(n))
+			case 'm'        : log.append(str(m))
+			case 'vs'       : log.append(str(vs))
+			case 'es'       : log.append(str(es))
+			case 'd'        : log.append(str(d))
+			case 'p'        : log.append(str(p))
+			case 'b'        : log.append(str(b))
+			case 'ws'       : log.append(str(ws))
+			case 'rs'       : log.append(str(rs))
+			case 'rs_valid' : log.append(str(rs_valid))
+			case 'de'       : log.append(str(de))
+			case 'pe'       : log.append(str(pe))
+			case 'be'       : log.append(str(be))
+			case 'wes'      : log.append(str(wes))
+			case 'res'      : log.append(str(res))
+			case 'res_valid': log.append(str(res_valid))
+			case _          : pass
+	print('\n\n'.join(log))
+
+def process(graph : str) -> str:
+	n, m = graph6_decode(graph)
+	vs = variable(n)
+
+	d = distance_matrix(m)
+	p = distance_similarity(d)
+	b = find(vs, p)
+
+	de, _ = distance_matrix_edge(n, m, d)
+	pe = distance_similarity(de)
+	be = find(vs, pe)
+
+	return json.dumps({
+		'graph': graph,
+		'metric_dimension': b,
+		'edge_dimension': be
+	})
+
+def main_process() -> None:
+	graphs = read_file(sys.stdin)
+	with multiprocessing.Pool() as pool:
+		for result in pool.imap_unordered(process, graphs):
+			print(result)
+
+def print_usage() -> None:
+	print(
+		re.sub(r'\n\t\t\t', r'\n',
+		'''
+			usage:
+				<debug|process> ...
+
+				debug <graph6 string> [info...]
+				process [info...]
+
+				The subcommand process read graphs from stdin.
+
+			info:
+				graph    : graph6 string
+
+				n        : number of vertices
+				m        : adjacency matrix
+				vs       : vertices\' names
+				es       : edges
+
+				d        : distance matrix
+				p        : distance similarity
+				b        : metric dimension
+				ws       : resolving sets
+				rs       : resolving sets\' representations
+				rs_valid : valid resolving sets?
+
+				de       : edge distance matrix
+				pe       : edge distance similarity
+				be       : edge metric dimension
+				wes      : edge resolving sets
+				res      : edge resolving sets\' representations
+				res_valid: valid edge resolving sets?
+		'''
+		).strip()
+	)
+
+def main(args : List[str]) -> None:
+	if len(args) < 1 or args[0] in ('debug') and len(args) < 2:
+		print_usage()
+		return
+	match args[0]:
+		case 'debug'  : main_debug(args[1], args[2:])
+		case 'process': main_process()
+		case _        : print_usage()
+
+__njit__distance_matrix_edge = nb.njit( # type: ignore
+	fastmath=True,
+	cache=True
+)(__njit__distance_matrix_edge)
+
+def run_main() -> None:
+	__njit__distance_matrix_edge.compile(( # type: ignore
+		nb.types.intp,
+		nb.types.Array(nb.types.boolean, 2, 'C'),
+		nb.types.Array(nb.types.intp, 2, 'C'),
+		nb.types.Array(nb.types.intp, 2, 'C')
+	))
+	main(sys.argv[1:])
+
+if __name__ == '__main__':
+	run_main()
