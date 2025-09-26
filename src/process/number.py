d3e01b8993a677b3f6ec6b1cffd3cb4df7713f88
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2025-09-26 21:40:54 +0000

    Convert number into a process

---
+++ src/process/number.py
@@ -0,0 +1,69 @@
+import json
+import re
+from typing import Any, Callable, cast, Dict, Tuple, Union
+
+preserve_order = False
+header = None
+
+def decode(result : str) -> Dict[str, Any]:
+	datum = cast(Dict[str, Any], json.loads(result))
+	return datum
+
+def make_transform(option : Tuple[Any, ...]) -> Callable[[Dict[str, Any]], Dict[str, Any]]:
+	regex, magnitude, precision, unit = cast(Tuple[str, int, int, str], option)
+	def transform(datum : Dict[str, Any]) -> Dict[str, Any]:
+		for key, value in datum.items():
+			if re.search(regex, key):
+				datum[key] = format_number(value, magnitude, precision, unit)
+		return datum
+	return transform
+
+def encode(datum : Dict[str, Any]) -> str:
+	return json.dumps(datum)
+
+option_spec = [
+	(['-r', '--regex'], '', None),
+	(['-o', '--magnitude'], 0, int),
+	(['-p', '--precision'], 3, int),
+	(['-u', '--unit'], '', None)
+]
+
+def option_valid(option : Tuple[Any, ...]) -> bool:
+	regex, _, _, _ = cast(Tuple[str, int, int, str], option)
+	if not regex:
+		return False
+	return True
+
+def help() -> str:
+	return re.sub(r'\n\t\t\t', r'\n',
+	'''
+		usage:
+			... <-r=<pattern>> [-m=<magnitude>] [-p=<precision>] [-u=<unit>]
+
+		options:
+			-r=<pattern>, --regex=<pattern>
+				Match on keys with this pattern.
+
+			-o=<magnitude>, --magnitude=<magnitude>
+				Multiplies by 10^o.
+				Defaults to o = 0.
+
+			-p=<precision>, --precision=<precision>
+				Round to p numbers after floating point.
+				Defaults to p = 3.
+
+			-u=<unit>, --unit=<unit>
+				Add unit after number.
+	'''
+	).strip()
+
+def format_number(x : float, o : int, p : int, u : str) -> Union[int, float, str]:
+	y : Union[int, float]
+	if p == 0:
+		y = int(10**o * x)
+	else:
+		y = round(10**o * x, p)
+	if u:
+		return '{}{}'.format(y, u)
+	else:
+		return y
