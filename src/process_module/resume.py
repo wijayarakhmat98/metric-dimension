c63aba42b12f591577e6b0fe97a9f4eb0546198c
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2025-10-03 10:57:53 +0000

    Set the number of threads for multiprocessing

--- src/process_module/resume.py
+++ src/process_module/resume.py
@@ -1,5 +1,6 @@
 import json
 import multiprocessing
+import os
 from pathlib import Path
 import re
 from typing import Any, cast, Dict, Optional, Set, Tuple
@@ -20,7 +21,7 @@ config_cache : Dict[int, Dict[str, Any]] = {}
 def transform(datum : Optional[Dict[str, Any]], option : Tuple[Any, ...]) -> Optional[Dict[str, Any]]:
 	if not datum:
 		return None
-	_, exclude = cast(Tuple[Path, Set[str]], option)
+	_, _, exclude = cast(Tuple[Path, int, Set[str]], option)
 	graph = cast(str, datum['graph'])
 	if graph in exclude:
 		return None
@@ -32,22 +33,30 @@ def encode(datum : Optional[Dict[str, Any]]) -> Optional[str]:
 	return json.dumps(datum)
 
 option_spec = [
-	(['-c', '--log'], '', Path)
+	(['-c', '--log'], '', Path),
+	(['-t', '--threads'], -1, int)
 ]
 
 def option_valid(option : Tuple[Any, ...]) -> bool:
-	log_filename, = cast(Tuple[Path], option)
+	log_filename, _ = cast(Tuple[Path, int], option)
 	if not log_filename:
 		return False
 	return True
 
 def option_augment(option : Tuple[Any, ...]) -> Tuple[Any, ...]:
-	log_filename, = cast(Tuple[Path], option)
+	log_filename, number_of_threads = cast(Tuple[Path, int], option)
+	if number_of_threads < 0:
+		number_of_threads = os.cpu_count() or 1
+		if 'THREADS' in os.environ:
+			try:
+				number_of_threads = int(os.environ['THREADS'])
+			except:
+				pass
 	with open(log_filename, 'r') as log_file:
 		log_read = read_file(log_file)
-		with multiprocessing.Pool() as pool:
+		with multiprocessing.Pool(processes=number_of_threads) as pool:
 			exclude = {graph for graph in set(pool.imap_unordered(extract_graph, log_read)) if graph}
-	option = (*option, exclude)
+	option = (log_filename, number_of_threads, exclude)
 	return option
 
 def help() -> str:
