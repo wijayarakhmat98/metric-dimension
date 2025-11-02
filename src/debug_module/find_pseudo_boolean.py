d569151fab49ffce4b62fa74c963d6ce42ee8c90
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2025-11-02 16:51:04 +0000

    Implement pseudo boolean algorithm

---
+++ src/debug_module/find_pseudo_boolean.py
@@ -0,0 +1,63 @@
+import metric_dimension
+import numpy as np
+from typing import Any, Tuple
+from utils import timer
+import z3 # type: ignore
+
+class timer_ms(timer):
+	def __str__(self) -> str:
+		return '{}ms'.format(round(1000 * float(self), 3))
+
+def debug(graph : str, option : Tuple[Any, ...]) -> None:
+	M = metric_dimension.graph6_decode(graph)
+	V = metric_dimension.vertices(M)
+	nV = len(V)
+	print('Graph: {}'.format(graph))
+	print('Vertices: {}'.format(nV))
+	print()
+
+	DV = metric_dimension.distance_matrix(M)
+	D = DV
+
+	print('Distance similarity...')
+	with timer_ms() as B_time: B = metric_dimension.distance_similarity(D)
+	print('Distance similarity time: {}'.format(B_time))
+	print()
+
+	print('Reduced distance similarity...')
+	with timer_ms() as P_time: P = metric_dimension.reduced_distance_similarity(B)
+	print('Reduced distance similarity time: {}'.format(P_time))
+	print()
+
+	X = np.array([z3.Bool('x{}'.format(v + 1)) for v in V]) # pyright: ignore
+	Y = np.array([(x, 1) for x in X])
+	s = z3.Solver()
+
+	print('Metric dimension...')
+	with timer_ms() as k_time:
+		for k in range(nV - 1, -1, -1):
+			print('Trying k = {}... '.format(k), end='', flush=True)
+			s.push()
+			Q = P[:, P.sum(axis=0) >= k]
+			COL = Q.shape[1]
+			s.add(z3.PbEq(Y, k)) # pyright: ignore
+			for i in range(COL):
+				s.add(z3.PbLe(Y[Q[:, i]], k - 1)) # pyright: ignore
+			with timer_ms() as k_time:
+				found : bool = s.check() == z3.sat # pyright: ignore
+			print(k_time)
+			if not found:
+				k = k + 1
+				break
+			s.pop()
+		else:
+			k = -1
+	print('Metric dimension: {}'.format(k))
+	print('Metric dimension time: {}'.format(k_time))
+	print()
+
+option_spec = None
+option_valid = None
+
+def help() -> str:
+	return ''
